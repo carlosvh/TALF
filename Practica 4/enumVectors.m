5d58f65a764e59186db27061257915c8e7c8c826
CanarioImperial <91335530+carlosvh@users.noreply.github.com>
2022-12-25 17:50:39 +0100

    Add files via upload

---
+++ Practica 4/enumVectors.m
@@ -0,0 +1,7 @@
+function element = enumVectors()
+  n = 0
+  while n >= 0
+    godeldecoding(n)
+    n = n+1
+  endwhile
+endfunction
---
+++ Practica 4/enumWhile.m
@@ -0,0 +1,7 @@
+function element = enumWhile()
+  n = 0
+  while n >= 0
+    N2WHILE(n)
+    n = n+1
+  endwhile
+endfunction
